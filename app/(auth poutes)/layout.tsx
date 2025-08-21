13741b184a5e7a22a565607b281a05cf6ca83a40
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-08-21 12:43:43 +0300

    tsx

--- app/(auth poutes)/layout.tsx
+++ app/(auth poutes)/layout.tsx
@@ -1,13 +1,12 @@
 
-import React from "react";
-import type { ReactNode } from "react";
+import React, { ReactNode } from "react";
 
 interface AuthLayoutProps {
     children: ReactNode;
-    modal?: boolean;
+
+
 }
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
-
     return <>{children}</>;
 }
