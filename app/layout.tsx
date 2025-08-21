60af1f8504f643eef5b34cd8b71bacfe512810c1
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-08-21 17:52:27 +0300

    tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,52 @@
-import { ReactNode } from "react";
-import QueryProvider from "@/components/QueryProvider/QueryProvider";
-
-interface RootLayoutProps {
-  children: ReactNode;
-}
-
-export default function RootLayout({ children }: RootLayoutProps) {
+import type { Metadata } from 'next';
+import Header from '@/components/Header/Header';
+import Footer from '@/components/Footer/Footer';
+import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
+import { Roboto } from 'next/font/google';
+import AuthProvider from '@/components/AuthProvider/AuthProvider';
+import 'modern-normalize';
+import './globals.css';
+export const metadata: Metadata = {
+  title: 'NoteHub',
+  description: 'A simple and efficient app for creating and organizing your notes.',
+  openGraph: {
+    title: 'NoteHub',
+    description: 'A simple and efficient app for creating and organizing your notes.',
+    url: `https://react-v3-hw-solutions.vercel.app/`,
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'NoteHub - A simple and efficient app for creating and organizing your notes.',
+      },
+    ],
+  },
+};
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['500', '600', '700'],
+  variable: '--font-roboto',
+  display: 'swap',
+});
+export default function RootLayout({
+  children,
+  modal,
+}: Readonly<{
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}>) {
   return (
     <html lang="en">
-      <body>
-        <QueryProvider>{children}</QueryProvider>
+      <body className={roboto.variable}>
+        <TanStackProvider>
+          <AuthProvider>
+            <Header />
+            {children}
+            {modal}
+            <Footer />
+          </AuthProvider>
+        </TanStackProvider>
       </body>
     </html>
   );
