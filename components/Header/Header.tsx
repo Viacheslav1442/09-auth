0c9fbe923a962517d5bc12af2ce2eec6f3e49fdf
Viacheslav1442 <viacheslav.verbytskyi@gmail.com>
2025-08-29 11:41:33 +0300

    tsx

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,14 +1,29 @@
 import css from "./Header.module.css";
-import AuthNavigation from "@/components/AuthNavigation/AuthNavigation";
+import Link from "next/link";
+import TagsMenu from "../TagsMenu/TagsMenu";
+import AuthNavigation from "../AuthNavigation/AuthNavigation";
 
-export default function Header() {
+const Header = () => {
     return (
         <header className={css.header}>
-            <nav className={css.navigation}>
-                <ul className={css.navigationList}>
+            <Link className={css.headerLink} href="/" aria-label="Home">
+                NoteHub
+            </Link>
+            <nav aria-label="Main Navigation">
+                <ul className={css.navigation}>
+                    <li className={css.navigationItem}>
+                        <Link className={css.navigationLink} href="/">
+                            Home
+                        </Link>
+                    </li>
+                    <li className={css.navigationItem}>
+                        <TagsMenu />
+                    </li>
                     <AuthNavigation />
                 </ul>
             </nav>
         </header>
     );
-}
+};
+
+export default Header;
